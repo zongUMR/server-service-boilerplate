f05a8c3e1e993d0cb998513c87394e12025f781c
geekberryonekey <158029544+geekberryonekey@users.noreply.github.com>
2024-03-11 19:56:43 +0800

    feat: defined mongo collection name by entity schema (#11)

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
+// XXX: default NODE_ENV "test" and "unittest" is conflict with online test server
+process.env.NODE_ENV = 'jest';
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
